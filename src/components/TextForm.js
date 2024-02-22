8e8be518204e05b6769037c05d06d0f8cd63c110
Jay Manek <manekj27@gmail.com>
2024-02-22 23:28:09 +0530

    Add word-character counter, time, preview 22-2-24

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,14 +11,28 @@ export default function TextForm(props) {
         console.log("Uppercase Was Clicked");
         setText(text.toUpperCase());
     }
-const [text,setText]= useState('Enter Text here');
+    const handleDownClick= ()=>{
+        console.log("Lowercase Was Clicked");
+        setText(text.toLowerCase());
+    }
+const [text,setText]= useState('');
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value= {text} onChange= {handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-       <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper case</button>
+       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Upper case</button>
+       <button className="btn btn-secondary mx-2" onClick={handleDownClick}>Convert to Lower case</button>
     </div>
+    <div className="container my-3">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
